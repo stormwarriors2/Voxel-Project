c241338772b9153306f0703842471a25404add17
Cameron Garchow <camerong124@gmail.com>
2019-04-27 14:05:43 -0500

    Project Update

--- Assets/Scripts/GenerateMesh.cs
+++ Assets/Scripts/GenerateMesh.cs
@@ -1,120 +1,17 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/// <summary>
-/// Voxel Script
-/// Spawns Voxels in terrian generation mimicking minecraft's style...
-/// Ideas were from all over the place on this one, but most of it is from what I saw in our voxel ocean, and our examples
-/// Also ideas are from the Example from Processing's Terrian Generation.
-/// For your answer about continually spawning chunks you tie it to the camera's location X / Z 
-/// the further you get away from a spawner, unloads / deletes the object and creates a new one around the player.
-/// Each Voxel is 4 quads, 4 quads are far easier for rendering than a cube. I could generate the cube mesh but it is far easier to do this.
-/// </summary>
-public class VoxelScript : MonoBehaviour
-{
-    /// <summary>
-    /// Current Voxel Type is the current voxel being used
-    /// </summary>
-    private GameObject currentVoxelType;
-    /// <summary>
-    /// Is an array of game objects that are pulled from the voxel
-    /// </summary>
-    public GameObject[] voxelColorType;
-    /// <summary>
-    /// Amp is the amplitude or height of the perlin noise
-    /// </summary>
-    [Range(5, 100)] public float amp = 10f;
-    /// <summary>
-    /// Is the frequency of spawning Voxels
-    /// </summary>
-    [Range(5, 100)] public float freq = 10f;
-    /// <summary>
-    /// Cols is the Columns and how many voxels are present HEIGHT
-    /// </summary>
-    [Range(50, 1000)] public int cols = 100;
-    /// <summary>
-    /// Rows is the rows of voxels WIDTH
-    /// </summary>
-    [Range(50, 1000)] public int rows = 100;
-    /// <summary>
-    /// Is the Position of the voxel spawner and each voxel
-    /// </summary>
-    private Vector3 Pos;
-    /// <summary>
-    /// Threshold for the Voxels
-    /// </summary>
-    [Range(2f, 100f)] public float thresh = 2f;
-    /// <summary>
-    /// Mesh on render
-    /// </summary>
-    MeshFilter mesh;
 
-    /// <summary>
-    ///  When Project starts generates the terrian
-    /// </summary>
+public class GenerateMesh : MonoBehaviour
+{
+    // Start is called before the first frame update
     void Start()
     {
-        generateTerrian();
+        Build();
     }
     /// <summary>
-    /// Generates the Terrian of Voxels
-    /// Calculates each Y coordinate and X coordinates and Z coords of blocks
-    /// Y is not SET by cols or rows, instead is set by a perlin noise field
+    /// Build Mesh
     /// </summary>
-    public void generateTerrian()
-    {
-       Pos = this.transform.position;
-        thresh = Perlin.Noise(1, 2, 50);
-        for (int x = 0; x < cols; x++)
-        {
-
-
-            for (int z = 0; z < rows; z++)
-            {
-
-                float y = Perlin.Noise((Pos.x + x) / freq, 0, (Pos.z + z) / freq) * amp;
-                Vector3 worldPosition = new Vector3(Pos.x + x, Pos.y + y, Pos.z + z);
-                Vector3 noiseCoords = worldPosition / thresh;
-                float density = Perlin.Noise(noiseCoords);
-                density -= worldPosition.y * thresh;
-               
-                y = Mathf.Floor(y);
-               
-                if (density > (freq / amp) * thresh) continue;
-                ///This was for fun wanted to see what happens if i made comparative statements with a perlin noise added in
-                if (y > x && y >= z)
-                {
-
-                 
-                    y = Perlin.Noise((Pos.x + x) / freq, (Pos.y + y), (Pos.z + z)) / freq;
-                }
-                if (y > amp / 2)
-                    {
-                    y = Perlin.Noise((Pos.x + x) / freq, (Pos.y + y), (Pos.z + z)) / freq;
-                    currentVoxelType = voxelColorType[0];
-
-                    }
-                    else if (y < amp / 2)
-                    {
-                        currentVoxelType = voxelColorType[1];
-                        if (y < amp / freq)
-                        {
-                        y = Perlin.Noise((Pos.x + x), (Pos.y + y), (Pos.z + z));
-                        currentVoxelType = voxelColorType[2];
-                        }
-                    }
-                
-                    GameObject newVoxel =
-                        GameObject.Instantiate(currentVoxelType, worldPosition, Quaternion.identity);
-
-                    newVoxel.transform.position =
-                        new Vector3(Pos.x + x, Pos.y + y, Pos.z + z);
-                }
-            
-        }
-    }
-
     public void Build()
     {
         List<CombineInstance> meshes = new List<CombineInstance>();
---
+++ Assets/Scripts/MeshCombine.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCombine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CombineMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void CombineMesh()
+    {
+        Quaternion rot = transform.rotation;
+        Vector3 pos = transform.position;
+
+        transform.rotation = Quaternion.identity;
+        transform.position = Vector3.zero;
+
+        MeshFilter[] filters = GetComponentsInChildren<MeshFilter>();
+
+        Mesh Meshfinal = new Mesh();
+
+
+        CombineInstance[] combineAll = new CombineInstance[filters.Length];
+
+
+
+        for (int i = 0; i < filters.Length; i++)
+        {
+            if(filters[i].transform == transform) continue;
+            combineAll[i].subMeshIndex = 0;
+            combineAll[i].mesh = filters[i].sharedMesh;
+            combineAll[i].transform = filters[i].transform.localToWorldMatrix;
+        } 
+
+        Meshfinal.CombineMeshes(combineAll);
+
+        GetComponent<MeshFilter>().sharedMesh = Meshfinal;
+
+        transform.rotation = rot;
+        transform.position = pos;
+    }
+}
